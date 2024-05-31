bcb4cc4f3907b7dd1e619b1bd21e0e348113260c
boraturbil <bora.turbil45@gmail.com>
2024-06-01 01:14:10 +0300

    wordle_fix

---
+++ WordleBio/Assets/Word Finder Main/Scripts/LetterContainer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class LetterContainer : MonoBehaviour
+{
+
+    [Header(" Elements ")]
+    [SerializeField] private SpriteRenderer letterContainer; 
+    [SerializeField] private TextMeshPro letter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Initialize()
+    {
+        letter.text = "";
+        letterContainer.color = Color.white;
+    }
+
+    public void SetLetter(char letter)
+    {
+        this.letter.text = letter.ToString(); 
+    }
+
+    public void SetValid()
+    {
+        letterContainer.color = Color.green;
+    }
+
+    public void SetPotential()
+    {
+        letterContainer.color = Color.yellow;
+    }
+
+    public void SetInvalid()
+    {
+        letterContainer.color = Color.gray;
+    }
+
+    public char GetLetter()
+    {
+        return letter.text[0];
+    }
+}
